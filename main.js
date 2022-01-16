cad8859af7224045bf794a08d2d9d17f825a2100
Belén Izquierdo dorado <59977724+bnizquierdo@users.noreply.github.com>
2022-01-16 22:32:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
+/*Preguntar al usuairo que elige para jugar*/
 var usuario = prompt('Elige piedra papel o tijera');
 
+/*Usar el metodo Maht para generar un número entre el 0 y el 2*/
+/*
+0=piedra
+1=papel
+2= tijera*/
 var maquina = Math.floor(Math.random() *3);
 
+/*Si el usuario elige piedra*/
 if (usuario === "piedra") {
+    /*Si la máquina escoge piedra*/
     if(maquina == 0){
         alert('Empatasteis. La máquina eligio piedra');
         
     }else{
+        /*Si la máquina escoge papel*/
         if(maquina == 1){
             alert('Perdiste. La máquina eligio papel');
         }else{
+            /*Si la máquina escoge tijera*/
             if(maquina == 2){
                 alert('Ganaste. La máquina eligio tijera');
             }
@@ -17,13 +27,18 @@ if (usuario === "piedra") {
     }
 }
 
+
+/*Si el usuario elige papel*/
 if (usuario === "papel") {
+    /*Si la máquina escoge piedra*/
     if(maquina == 0){
         alert('Ganaste. La máquina eligio piedra');
     }else{
+        /*Si la máquina escoge papel*/
         if(maquina == 1){
             alert('Empatasteis. La máquina eligio papel');
         }else{
+            /*Si la máquina escoge tijera*/
             if(maquina == 2){
                 alert('Perdiste. La máquina eligio tijera');
             }
@@ -31,14 +46,17 @@ if (usuario === "papel") {
     }
 }
 
+/*Si el usuario elige tijera*/
 if (usuario === "tijera") {
 
     if(maquina == 0){
         alert('Perdiste. La máquina eligio piedra');
     }else{
+        /*Si la máquina escoge papel*/
         if(maquina == 1){
             alert('Ganaste. La máquina eligio papel');
         }else{
+            /*Si la máquina escoge tijera*/
             if(maquina == 2){
                 alert('Empatasteis. La máquina eligio tijera');
             }
